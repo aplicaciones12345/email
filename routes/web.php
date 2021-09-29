7a086a290d71d2c85a696f5570ae25f7a79a6c3c
xabi <cursoaplicaciones220@gmail.com>
2021-09-29 12:02:17 +0200

    Añadida la ruta contact en archivo web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('test'); //Que es el formulario de contacto
 });
+
+//Ruta a la que está señalando el formulario de contacto
+Route::post('/contactar', 'App\Http\Controllers\EmailController')->name('contact');
